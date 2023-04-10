e264eed6a711c22ab8338f5dc42e160791f36ddc
Ezlifa Shah <ezlifashah123@gmail.com>
2023-04-10 15:55:43 -0700

    java task

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+// first task 
+
+ var zakatpercentage = 0.025;
+ var userinput = +prompt("Enter Your Amount");
+ var result = zakatpercentage = userinput ;
+ alert ("Your Zakat Value is" + result  );
+
+
+
+
+// second task 
+
+
+alert('Hello World')
+var username =prompt("Your Name PLease","Ezlifa Shah")
+var user = "Ezlifa"
+var fuser = "Shah"
+console.log("Hello " + user+" " +fuser)
+
+var ezma jawn 
+ezma jawn camelCase
+ezma jawn PascalCase
+ezma jawn snake_case
+
+console.log(2+5)
+console.log(2-5)
+console.log(2*5)
+console.log(2/5)
+console.log(2**5)
+
+var exp = (8 + (7 / 9) - (10 / 7) * (4 - 6)) + 4
+console.log(exp)
+
+var age =+ prompt("Your age PLease")
+var fage =+ prompt("Your father age PLease")
+alert(age+fage)
+alert(Number(age) + Number(fage))
+console.log(typeof age)
+
