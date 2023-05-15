56bf81c6ce28649f74743ac0c45dd29344b8e7bf
Ezlifa Shah <ezlifashah123@gmail.com>
2023-05-15 11:25:00 -0700

    8 task

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,96 @@
-// first task 
+first task 
 
- var zakatpercentage = 0.025;
- var userinput = +prompt("Enter Your Amount");
- var result = zakatpercentage = userinput ;
- alert ("Your Zakat Value is" + result  );
-
-
+var zakatpercentage = 0.025;
+var userinput = +prompt("Enter Your Amount");
+var result = zakatpercentage = userinput;
+alert("Your Zakat Value is" + result);
 
 
-// second task 
+
+
+1st task 
+
+
+
+var familyMembers = + prompt("Enter the total number of your family members");
+// var familyMembers =(Number(familyMembers))
+var fitrahmethod = prompt("Select your fitrah method among the options type option number:1) Wheat Rs 250, 2) Barley Rs 450, 3) Dates 2100, 4) Raisins 2800");
+var price
+if (fitrahmethod === "1") {
+    price = 250;
+} else if (fitrahmethod === "2") {
+    price = 450;
+} else if (fitrahmethod === "3") {
+    price = 2100;
+} else if (fitrahmethod === "4") {
+    price = 2800;
+} else {
+    alert("Option Invalid/Incorrect")
+}
+var fitrahAmount = price * familyMembers
+alert('Fitrah Amount of your family members is ' + result)
+
+task 2nd
+
+var WorldCupSquad =[player1,player2,player3,player4,player5,player6,player7,player8,player9,player10,player11,player12,player13,player14,player15]
+console.log(WorldCupSquad)
+var finalteamplayers= WorldCupSquad.slice(2,13)
+console.log(finalteamplayers)
+
+task 3rd
+
+var nationality =prompt("Your Nationality Please")
+var gender =prompt("Your Gender Please")
+var age =+prompt ("Your Age Please")
+if(nationality==='Pakistani'||nationality==='pakistani'||nationality==='Indian'|| nationality==='indian')
+{alert("You are eligible to vote")
+}else if(gender===female && age>=18);
+var marritalStatus = prompt("Are you married? Yes or No");
+if(marritalStatus ==="yes"){
+    alert("You are eligible to vote");
+}
+else{
+    alert("You are not eligible to vote");
+}
+
+task 4th
 
 
 alert('Hello World')
-var username =prompt("Your Name PLease","Ezlifa Shah")
+var username = prompt("Your Name PLease", "Ezlifa Shah")
 var user = "Ezlifa"
 var fuser = "Shah"
-console.log("Hello " + user+" " +fuser)
+console.log("Hello " + user + " " + fuser)
 
 var ezma jawn 
 ezma jawn camelCase
 ezma jawn PascalCase
 ezma jawn snake_case
 
-console.log(2+5)
-console.log(2-5)
-console.log(2*5)
-console.log(2/5)
-console.log(2**5)
+console.log(2 + 5)
+console.log(2 - 5)
+console.log(2 * 5)
+console.log(2 / 5)
+console.log(2 ** 5)
 
 var exp = (8 + (7 / 9) - (10 / 7) * (4 - 6)) + 4
 console.log(exp)
 
-var age =+ prompt("Your age PLease")
-var fage =+ prompt("Your father age PLease")
-alert(age+fage)
+var age = + prompt("Your age PLease")
+var fage = + prompt("Your father age PLease")
+alert(age + fage)
 alert(Number(age) + Number(fage))
 console.log(typeof age)
 
+task 5th
+
+var secretnumber=+promt ("guess the secretnumber between (1 to 10");
+if (secretnumber=10){
+alert("you did it");
+ }
+else if(secretnumber<=10){
+alert("smart you are very near");
+}
+else{
+alert("guess again");
+}
